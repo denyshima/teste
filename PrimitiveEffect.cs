7e49b1bf90a9bc65ba51eefc8b4eeed0e3d6e44f
igor-quillis <38540913+igor-quillis@users.noreply.github.com>
2018-05-02 16:48:00 -0300

    Update PrimitiveEffects with comments

--- PrimitiveEffect.cs
+++ PrimitiveEffect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Simple death effect (destroys object at the end)
 public class PrimitiveEffect : MonoBehaviour {
     private Material mat_materialToFade;    
     public float f_fadeSpeed;
@@ -9,31 +10,39 @@ public class PrimitiveEffect : MonoBehaviour {
 
     void Start()
     {
+        //Get the material of this gameObject
         mat_materialToFade = gameObject.GetComponent<Renderer>().material;       
     }
-
+    
     void FixedUpdate()
     {
         StartCoroutine("FadeOut");
         StartCoroutine("Rise");
+        
+        //"Kills" the gameObject when it goes completely transparent
         if (mat_materialToFade.color.a <= 0)
         {
             Destroy(gameObject);
         }
     }
-
+    
+    //Speed adjustable by public float f_fadeSpeed
     IEnumerator FadeOut()
     {
+        //While the material is still visible, make it less visible according to time passed
         while (mat_materialToFade.color.a > 0)
         {
             Color color_newColor = mat_materialToFade.color;
             color_newColor.a -= Time.deltaTime;
             mat_materialToFade.color = color_newColor;
-            gameObject.GetComponent<Renderer>().material = mat_materialToFade;            
+            gameObject.GetComponent<Renderer>().material = mat_materialToFade;
+            /* Instead of the above 2 lines, it could be done as only one line as follows:
+            gameObject.GetComponent<Renderer>().material.color = color_newColor; */
             yield return new WaitForSeconds(f_fadeSpeed);
         }
-    }
-
+    }   
+    
+    //Speed adjustable by public float f_riseSpeed
     IEnumerator Rise()
     {
         transform.Translate(0f,Time.deltaTime,0f);
