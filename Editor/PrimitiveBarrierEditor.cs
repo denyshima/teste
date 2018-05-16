6eb9267c5fc3005456c7614a2d03a8341a20d4d8
igor-quillis <38540913+igor-quillis@users.noreply.github.com>
2018-05-16 14:37:52 -0300

    Add Folder and files
    Testing scripts for customizing the Editor in unity

---
+++ Editor/PrimitiveBarrierEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrimitiveBarrier))]
+public class PrimitiveBarrierEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EditorGUILayout.HelpBox("Here we can write tips for using this script in the editor mode! Very useful. To modify this, go to the Editor folder, PrimitiveBarrierEditor script.", MessageType.Info);
+    }
+}
---
+++ Editor/PrimitiveEffectsEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// Attribute that enables custom editor features
+[CustomEditor(typeof(PrimitiveEffect))]
+public class PrimitiveEffectsEditor : Editor {
+
+    // Note that this is not a MonoBehaviour, but an Editor child
+    // Bellow we override the OnInspectorGUI that unity uses as default for this script only
+    public override void OnInspectorGUI()
+    {
+        // We choose to draw the default inspector anyway, since we just want to add things
+        DrawDefaultInspector();
+        // Now we add a helpbox in the inspector
+        EditorGUILayout.HelpBox("Look for the Editor folder, PrimitiveEffectsEditor script, to understand how it works! We can use it for more things, like adding a custom field that is not a PUBLIC variable!", MessageType.Info);
+    }
+}
